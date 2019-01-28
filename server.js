64248da6cda7d074168fb955cf642ca67309fb33
AVWORLD\rob9575 <rsantos@esri.com>
2019-01-28 10:49:10 -0600

    Baseline node server

---
+++ server.js
@@ -0,0 +1,57 @@
+//this is the "app file," the project's entry point
+
+var express = require('express');
+
+var app = express();
+
+//setup handlebars view engine
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+//if env.PORT exists, use that.  If not, use 3000
+app.set('port', process.env.PORT || 3000);
+
+//add the public directory to serve it to client:
+app.use(express.static(__dirname+'/public'));
+
+//use cors
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+/**
+ * Routes
+ * app.VERB ignores trailing slashes, things after ? for query strings, etc.  it just works
+ */
+app.get('/', function(req, res) {
+    res.render('home');
+});
+
+/**
+ * Handlers
+ * express has more robust methods and attributes for handling responses and requests
+ */
+
+//custom 404 page
+//app.use is the method by which express adds middleware, which is used for error handling
+//in this case, it acts as a catchall for anything that doesn't match a route
+app.use(function(req, res) {
+    res.status(404);
+    res.render('404');
+});
+
+//custom 500 page
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Express started on localhost:' + app.get('port')+ '; press Ctrl-C to terminate.');
+});
+
+
